10baa6c9b79dd81bb0d64f144764935ea277bfe2
Kyriacos-Stavrinides <51713787+Kyriacos-Stavrinides@users.noreply.github.com>
2022-12-15 18:33:41 +0200

    basically finished

--- app.js
+++ app.js
@@ -1,1 +1,13 @@
-console.log('test')
+const accordionButtons = Array.from(document.getElementsByClassName('accordion-header'));
+
+function textOpen(target) {
+    console.log(target.firstElementChild)
+    target.firstElementChild.classList.toggle('font-bold')
+    target.firstElementChild.classList.toggle('collapsed')
+    target.nextElementSibling.classList.toggle('show') 
+}
+
+accordionButtons.forEach(element => {
+    element.addEventListener('click', (e) => textOpen(element))
+});
+    
